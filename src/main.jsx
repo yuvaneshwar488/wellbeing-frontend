b972d4b94fbd5687073632c425db12c472e143d9
yuvaneshwar488 <yuvangenspark@gmail.com>
2026-09-26 01:02:28 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WellbeingApplicationUnified from './App.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <WellbeingApplicationUnified />
+  </React.StrictMode>
+);
